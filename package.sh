c760a7c7d1f37348b02817d88f1f70caf494f558
CrystalJiang232 <yexiaozhou200503@163.com>
2026-07-25 21:54:18 +0800

    Packaging utils & shell script

---
+++ package.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source /etc/profile
+
+mkdir -p bin
+for dir in */; do
+    base="${dir%/}"
+    for i in tmp bak bin utils; do [[ "$base" == *"$i"* ]] && continue 2; done
+    target="bin/${base}.skill"
+    if [[ -e "$target" ]]; then
+        n=1
+        while [[ -e "${target}.bak.${n}" ]]; do
+            ((n++))
+        done
+        mv "$target" "${target}.bak.${n}"
+    fi
+    python utils/package_skill.py $base bin/
+done
